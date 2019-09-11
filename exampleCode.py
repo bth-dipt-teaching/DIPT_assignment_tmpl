638bf25bd1b582c5a1109cd164f5380c369821d6
Unknown DIPT member <noreply@bth.se>
2019-09-11 08:31:28 +0200

    Version 1.2

---
+++ exampleCode.py
@@ -0,0 +1,9 @@
+# Python3 example for adding two numbers 
+number1 = 15
+number2 = 27
+
+# Add the two numbers
+sum = number1 + number2 
+
+# Print values and result
+print("The sum of {0} and {1} is {2}" .format(number1, number2, sum)) 
